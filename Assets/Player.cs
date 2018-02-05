c00ef8696beee0dad7a134677407c41951c5ba03
dantogno <vebulidbih@msn.com>
2018-02-05 09:56:18 -0600

    finished demo

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float speed = 1;
 
+    [SerializeField]
+    private float damageCooldown = 2;
+
     private float maxHitpoints = 100;
     private float currentHitpoints;
-
+    private bool isInvulnerable;
     private Rigidbody2D rigidbody;
     private float horizontalInput, verticalInput;
+    private SpriteRenderer spriteRenderer;
 
 
     public float CurrentHitpoints
@@ -21,6 +25,15 @@ public class Player : MonoBehaviour
         {
             return currentHitpoints;
         }
+
+        private set
+        {
+            currentHitpoints = value;
+
+            if (currentHitpoints < 0)
+                currentHitpoints = 0;
+        }
+
     }
 
     public float CurrentHitpointsAsPercentage
@@ -31,20 +44,28 @@ public class Player : MonoBehaviour
         }
     }
 
-
     // Use this for initialization
     void Start () 
 	{
         currentHitpoints = maxHitpoints;
         rigidbody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () 
 	{
         GetInput();
         HandleMovement();
 	}
+
+    private IEnumerator HandleDamageCooldown()
+    {
+        isInvulnerable = true;
+        StartCoroutine(DoBlinkInvulnerableEffect());
+        yield return new WaitForSeconds(damageCooldown);
+        isInvulnerable = false;
+    }
 
     private void HandleMovement()
     {
@@ -60,6 +81,26 @@ public class Player : MonoBehaviour
 
     public void TakeDamage(float amount)
     {
-        currentHitpoints -= amount;
+        if (!isInvulnerable)
+        {
+            CurrentHitpoints -= amount;
+            StartCoroutine(HandleDamageCooldown());
+        }
+    }
+
+    private IEnumerator DoBlinkInvulnerableEffect()
+    {
+        Color transparentColor = new Color(1, 1, 1, 0);
+        Color regularColor = spriteRenderer.color;
+
+        float blinkInterval = 0.25f;
+
+        while (isInvulnerable)
+        {
+            spriteRenderer.color = transparentColor;
+            yield return new WaitForSeconds(blinkInterval);
+            spriteRenderer.color = regularColor;
+            yield return new WaitForSeconds(blinkInterval);
+        }
     }
 }
